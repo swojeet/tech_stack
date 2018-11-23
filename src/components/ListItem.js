9fb2e9cdfb439a869c203dca0393793093f4fb19
swojeet <swojeetkayastha@gmail.com>
2018-11-23 16:51:32 +0545

    Add style to library title and parent view tag

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,13 +4,22 @@ import { CardSection } from './common';
 
 class ListItem extends Component {
   render() {
-    console.log(this.props.library.title);
+    const { titleStyle } = styles;
     return (
       <CardSection>
-        <Text>{this.props.library.title}</Text>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
       </CardSection>
     );
   }
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
